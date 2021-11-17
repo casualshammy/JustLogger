9aa459e3b6c079f63b5304ac8cd48fadc1839f99
casualshammy <axio@axio.name>
2021-11-17 20:34:02 +0000

    Build script fix

--- build.ps1
+++ build.ps1
@@ -3,4 +3,4 @@ dotnet restore
 dotnet build -c Release -o _package
 $tag = git describe --tags --abbrev=0
 dotnet pack -c Release /p:Version=$tag -o _package
-dotnet nuget push _package/*.nupkg --api-key $env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
+dotnet nuget push _package\*.nupkg --api-key $env:NUGET_API_KEY --source https://api.nuget.org/v3/index.json
