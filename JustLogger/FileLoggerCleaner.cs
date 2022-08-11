5294301f3b9451ffed675fdca394c343fd8b1821
casualshammy <axio@axio.name>
2022-08-11 20:49:12 +0100

    fix (NamedLogger): now correctly build composite name of log

--- JustLogger/FileLoggerCleaner.cs
+++ JustLogger/FileLoggerCleaner.cs
@@ -4,62 +4,67 @@ using System.IO;
 using System.Text.RegularExpressions;
 using System.Timers;
 
-namespace JustLogger
+namespace JustLogger;
+
+public class FileLoggerCleaner : IDisposable
 {
-    public class FileLoggerCleaner : IDisposable
+  private readonly Timer p_rotateTimer;
+  private readonly DirectoryInfo p_directory;
+  private readonly bool p_recursive;
+  private readonly Regex p_rotateFileNamePattern;
+  private readonly TimeSpan p_rotateInterval;
+  private bool p_disposedValue;
+
+  private FileLoggerCleaner(DirectoryInfo _directory, bool _recursive, Regex _rotateFileNamePattern, TimeSpan _rotateInterval)
+  {
+    p_rotateTimer = new Timer(10 * 60 * 1000);
+    p_rotateTimer.Elapsed += RotateTimer_Elapsed;
+    p_rotateTimer.Start();
+    p_directory = _directory;
+    p_recursive = _recursive;
+    p_rotateFileNamePattern = _rotateFileNamePattern;
+    p_rotateInterval = _rotateInterval;
+  }
+
+  /// <summary>
+  /// Creates log watcher. Logs in <paramref name="_directory"/> older than <paramref name="_rotateInterval"/> will be purged every 10 minutes
+  /// </summary>
+  public static FileLoggerCleaner Create(DirectoryInfo _directory, bool _recursive, Regex _logFileNamePattern, TimeSpan _rotateInterval)
+      => new(_directory, _recursive, _logFileNamePattern, _rotateInterval);
+
+  private void RotateTimer_Elapsed(object _sender, ElapsedEventArgs _e)
+  {
+    var now = DateTime.UtcNow;
+    var files = p_recursive ?
+        p_directory.GetFiles("*.*", SearchOption.AllDirectories) :
+        p_directory.GetFiles("*.*", SearchOption.TopDirectoryOnly);
+
+    foreach (var file in files)
+      if ((now - file.LastWriteTimeUtc) > p_rotateInterval && p_rotateFileNamePattern.IsMatch(file.Name))
+        try
+        {
+          file.Delete();
+        }
+        catch { }
+  }
+
+  protected virtual void Dispose(bool _disposing)
+  {
+    if (!p_disposedValue)
     {
-        private readonly Timer p_rotateTimer;
-        private readonly DirectoryInfo p_directory;
-        private readonly bool p_recursive;
-        private readonly Regex p_rotateFileNamePattern;
-        private readonly TimeSpan p_rotateInterval;
-        private bool p_disposedValue;
-
-        public FileLoggerCleaner(DirectoryInfo _directory, bool _recursive, Regex _rotateFileNamePattern, TimeSpan _rotateInterval)
-        {
-            p_rotateTimer = new Timer(10 * 60 * 1000);
-            p_rotateTimer.Elapsed += RotateTimer_Elapsed;
-            p_rotateTimer.Start();
-            p_directory = _directory;
-            p_recursive = _recursive;
-            p_rotateFileNamePattern = _rotateFileNamePattern;
-            p_rotateInterval = _rotateInterval;
-        }
-
-        private void RotateTimer_Elapsed(object sender, ElapsedEventArgs e)
-        {
-            var now = DateTime.UtcNow;
-            var files = p_recursive ?
-                p_directory.GetFiles("*.*", SearchOption.AllDirectories) :
-                p_directory.GetFiles("*.*", SearchOption.TopDirectoryOnly);
-
-            foreach (var file in files)
-                if ((now - file.LastWriteTimeUtc) > p_rotateInterval && p_rotateFileNamePattern.IsMatch(file.Name))
-                    try
-                    {
-                        file.Delete();
-                    }
-                    catch { }
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!p_disposedValue)
-            {
-                if (disposing)
-                {
-                    p_rotateTimer.Stop();
-                    p_rotateTimer.Dispose();
-                }
-                p_disposedValue = true;
-            }
-        }
-
-        public void Dispose()
-        {
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
-        }
-
+      if (_disposing)
+      {
+        p_rotateTimer.Stop();
+        p_rotateTimer.Dispose();
+      }
+      p_disposedValue = true;
     }
+  }
+
+  public void Dispose()
+  {
+    Dispose(_disposing: true);
+    GC.SuppressFinalize(this);
+  }
+
 }
--- JustLogger/NamedLogger.cs
+++ JustLogger/NamedLogger.cs
@@ -3,60 +3,63 @@ using JustLogger.Interfaces;
 using JustLogger.Toolkit;
 using System;
 
-namespace JustLogger
+namespace JustLogger;
+
+public class NamedLogger : ILogger
 {
-    public class NamedLogger : ILogger
-    {
-        private readonly ILogger p_logger;
-        private readonly string p_name;
+  private readonly ILogger p_logger;
+  private readonly string p_name;
 
-        public NamedLogger(ILogger logger, string name)
-        {
-            if (string.IsNullOrEmpty(name)) throw new ArgumentException($"'{nameof(name)}' cannot be null or empty.", nameof(name));
-            p_logger = logger ?? throw new ArgumentNullException(paramName: nameof(logger));
-            p_name = name;
-        }
+  public NamedLogger(ILogger logger, string name)
+  {
+    if (string.IsNullOrEmpty(name)) throw new ArgumentException($"'{nameof(name)}' cannot be null or empty.", nameof(name));
+    p_logger = logger ?? throw new ArgumentNullException(paramName: nameof(logger));
 
-        public void Info(string text, string? overrideName = null)
-        {
-            p_logger.Info(text, overrideName ?? p_name);
-        }
+    if (logger is not NamedLogger namedLogger)
+      p_name = name;
+    else
+      p_name = $"{namedLogger.p_name}//{name}";
+  }
 
-        public void Warn(string text, string? overrideName = null)
-        {
-            p_logger.Warn(text, overrideName ?? p_name);
-        }
+  public void Info(string text, string? overrideName = null)
+  {
+    p_logger.Info(text, overrideName ?? p_name);
+  }
 
-        public void Error(string text, string? overrideName = null)
-        {
-            p_logger.Error(text, overrideName ?? p_name);
-        }
+  public void Warn(string text, string? overrideName = null)
+  {
+    p_logger.Warn(text, overrideName ?? p_name);
+  }
 
-        public void Error(string text, Exception _ex, string? overrideName = null)
-        {
-            p_logger.Error(text, _ex, overrideName ?? p_name);
-        }
+  public void Error(string text, string? overrideName = null)
+  {
+    p_logger.Error(text, overrideName ?? p_name);
+  }
 
-        public void NewEvent(LogEntryType type, string text)
-        {
-            if (type == LogEntryType.INFO)
-                Info(text);
-            else if (type == LogEntryType.WARN)
-                Warn(text);
-            else if (type == LogEntryType.ERROR)
-                Error(text);
-        }
+  public void Error(string text, Exception _ex, string? overrideName = null)
+  {
+    p_logger.Error(text, _ex, overrideName ?? p_name);
+  }
 
-        public long GetEntriesCount(LogEntryType type)
-        {
-            return p_logger.GetEntriesCount(type);
-        }
+  public void NewEvent(LogEntryType type, string text)
+  {
+    if (type == LogEntryType.INFO)
+      Info(text);
+    else if (type == LogEntryType.WARN)
+      Warn(text);
+    else if (type == LogEntryType.ERROR)
+      Error(text);
+  }
 
-        public NamedLogger this[string name] => new(this, name);
+  public long GetEntriesCount(LogEntryType type)
+  {
+    return p_logger.GetEntriesCount(type);
+  }
 
-        public void Flush()
-        {
-            p_logger.Flush();
-        }
-    }
+  public NamedLogger this[string name] => new(this, name);
+
+  public void Flush()
+  {
+    p_logger.Flush();
+  }
 }
